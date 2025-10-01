6790835d71cdd5ccd0954e1961599fae9b87ced4
David CHOCHO PC <davidchchnewboy@gmail.com>
2025-10-01 16:20:27 +0200

    base des routes flask

--- api.py
+++ api.py
@@ -11,5 +11,33 @@ def home():
 def hello():
     return "<h1>Bienvenue au nouveau arrivant ! </h1>"
 
+@app.route('/residents', methods=['GET'])
+def residents():
+    pass
+
+@app.route('/residents/<id>', methods=['GET'])
+def get_resident(id: str):
+    pass
+
+@app.route('/residents', methods=['POST'])
+def add_resident():
+    pass
+
+@app.route('/residents/<id>', methods=['PUT'])
+def update_resident(id: str):
+    pass
+
+@app.route('/residents/<id>', methods=['DELETE'])
+def delete_resident(id: str):
+    pass
+
+@app.route('/eligible', methods=['GET'])
+def get_eligibles():
+    pass
+
+@app.route('/attributions', methods=['POST'])
+def create_attributions():
+    pass
+
 if __name__ == '__main__':
     app.run(port=5050, use_reloader=True)
