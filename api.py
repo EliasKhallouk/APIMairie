63427125baebe2dad855d0e14179fc9654b0d9ab
EliasKhallouk <eliaskhallouk@gmail.com>
2025-10-01 16:24:24 +0200

    route residents

--- api.py
+++ api.py
@@ -5,15 +5,16 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify({"message": "Welcome to the API!"})
-
-@app.route('/hello/<name>', methods=['GET'])
-def hello():
     return "<h1>Bienvenue au nouveau arrivant ! </h1>"
 
-@app.route('/residents', methods=['GET'])
+@app.route('/api/v1/residents', methods=['GET'])
 def residents():
-    pass
+    residents = [
+        {"id": 1, "prenom": "John", "age": 30, "date_arrivee": "2020-01-15"},
+        {"id": 2, "prenom": "Jane ", "age": 25, "date_arrivee": "2021-06-10"},
+        {"id": 3, "prenom": "Alice", "age": 28, "date_arrivee": "2019-11-20"}
+    ]
+    return jsonify(residents)
 
 @app.route('/residents/<id>', methods=['GET'])
 def get_resident(id: str):
